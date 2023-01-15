3a0e9e6b93c6e2dffa7e6b2152199faef27bf794
t4sk <tsk.nakamura@gmail.com>
2023-01-15 16:03:02 +0900

    comment

--- src/14-gate-keeper-two.sol
+++ src/14-gate-keeper-two.sol
@@ -7,6 +7,21 @@ interface IGateKeeperTwo {
 
 contract Hack {
     constructor(IGateKeeperTwo target) {
+        // Bitwise xor
+        // a     = 1010
+        // b     = 0110
+        // a ^ b = 1100
+
+        // a ^ a ^ b = b
+
+        // a     = 1010
+        // a     = 1010
+        // a ^ a = 0000
+
+        // max = 11...11
+        // s ^ key = max
+        // s ^ s ^ key = s ^ max 
+        //         key = s ^ max 
         uint64 s = uint64(bytes8(keccak256(abi.encodePacked(address(this)))));
         uint64 k = type(uint64).max ^ s;
         bytes8 key = bytes8(k);
